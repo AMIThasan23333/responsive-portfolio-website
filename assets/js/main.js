e1c6bdb95c42bc0cf626a34c24d5fed01ab5f726
My Name <cpgsom819@gmail.com>
2023-05-12 16:29:11 +0800

    addni responsive nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,41 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu')
+      navToggle = document.getElementById('nav-toggle')
+      navClose =document.getElementById('nav-close')
+   
 
+      if(navToggle){
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+            console.log("first")
+        })
+      }
+
+      if(navClose){
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu')
+            console.log("first")
+        })
+      }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(N => N.addEventListener('click', linkAction))
+
+
+
+
+
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
